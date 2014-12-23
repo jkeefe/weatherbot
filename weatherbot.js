2d4404a65a91fbf07c5844d77c309f1d1327b1b1
John Keefe <john@johnkeefe.net>
2014-12-22 22:39:45 -0500

    remove key echo

--- weatherbot.js
+++ weatherbot.js
@@ -11,8 +11,6 @@ var Bot = new Twit({
 	access_token_secret:  keys.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-console.log(keys.FORECAST_IO_API_KEY);
-
 // Listen To Twitter Stream filtered for @DataNewsWeather
 var stream = Bot.stream('statuses/filter', { track: ['@HiWeatherbot'] });
 
